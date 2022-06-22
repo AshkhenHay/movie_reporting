70dbdf9d3a985d193add45b4c6e1dfe18d920697
Ashkhen Hayrapetyan <ahayrapetyan@zscaler.com>
2022-06-22 12:30:28 +0400

    integrated Eureka

--- src/main/java/com/epam/movie_reporting/config/SwaggerConfig.java
+++ src/main/java/com/epam/movie_reporting/config/SwaggerConfig.java
@@ -2,8 +2,6 @@ package com.epam.movie_reporting.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
